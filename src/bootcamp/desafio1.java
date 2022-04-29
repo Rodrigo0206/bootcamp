464ef1597daeed1dfe40cb385271964b00780a82
unknown <rodrigo.domingues.87@hotmail.com>
2022-04-29 17:05:21 -0300

    prod a e b

--- src/bootcamp/desafio1.java
+++ src/bootcamp/desafio1.java
@@ -1,5 +1,18 @@
 package bootcamp;
-
-public class desafio1 {
-
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, PROD;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		PROD =  ; //implemente o codigo
+		
+		System.out.println("PROD = " + );
+		
+		sc.close();
+	}
 }
